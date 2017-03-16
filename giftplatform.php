572672d6ccb4f8b1cf679bf2847a15e7307a8b5f
Ben Bedwell <github@growlingfish.com>
2017-03-16 15:50:46 +0000

    Plugin

---
+++ giftplatform.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       GIFT platform
+ * Plugin URI:        https://github.com/growlingfish/giftplatform
+ * Description:       GIFT project digital gifting platform
+ * Version:           0.0.0.1
+ * Author:            Ben Bedwell
+ * License:           GNU General Public License v3
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       giftplatform
+ * GitHub Plugin URI: https://github.com/growlingfish/giftplatform
+ * GitHub Branch:     master
+ */
+ 
+ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+ 
+ 
+ 
+ ?>
