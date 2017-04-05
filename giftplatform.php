f32304fd19daaea79e8d956c518f48cbfea591b7
growlingfish <github@growlingfish.com>
2017-04-05 13:55:58 +0100

    Admin

--- giftplatform.php
+++ giftplatform.php
@@ -3,7 +3,7 @@
  * Plugin Name:       GIFT platform plugin
  * Plugin URI:        https://github.com/growlingfish/giftplatform
  * Description:       WordPress admin and server for GIFT project digital gifting platform
- * Version:           0.0.0.1
+ * Version:           0.0.0.2
  * Author:            Ben Bedwell
  * License:           GNU General Public License v3
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
@@ -12,8 +12,29 @@
  * GitHub Branch:     master
  */
  
- defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
  
+register_activation_hook( __FILE__, 'giftplatform_activate' );
+function giftplatform_activate () {
+	flush_rewrite_rules();
+}
  
- 
- ?>
+/*
+*	Simplify
+*/
+
+add_action('init', 'giftplatform_remove_categories');
+function giftplatform_remove_categories () {
+	register_taxonomy('category', array());
+}
+
+add_action('admin_menu', 'giftplatform_remove_admin_options');
+function giftplatform_remove_admin_options () {
+	//if (!current_user_can('manage_options')) {
+		remove_menu_page( 'edit.php' );
+		remove_menu_page( 'edit.php?post_type=page' );
+		remove_menu_page( 'edit-comments.php' );
+    //}
+}
+
+?>
