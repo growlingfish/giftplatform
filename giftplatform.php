141fda5a9e91e88306fb412b7f966dabd377d8da
Ben Bedwell <github@growlingfish.com>
2017-07-04 09:03:02 +0100

    API

--- giftplatform.php
+++ giftplatform.php
@@ -3,7 +3,7 @@
  * Plugin Name:       GIFT platform plugin
  * Plugin URI:        https://github.com/growlingfish/giftplatform
  * Description:       WordPress admin and server for GIFT project digital gifting platform
- * Version:           0.0.1.5
+ * Version:           0.0.1.6
  * Author:            Ben Bedwell
  * License:           GNU General Public License v3
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
@@ -217,7 +217,14 @@ function get_gifts ($request) {
 					$wrap->unwrap_place = html_entity_decode(get_field('place', $wrap->ID));
 					$wrap->unwrap_artcode = get_field('artcode', $wrap->ID);
 					$wrap->unwrap_personal = get_field('personal', $wrap->ID);
-					$wrap->unwrap_object = get_field('object', $wrap->ID);
+					$objects = get_field('object', $wrap->ID);
+					foreach ($objects as $object) {
+						$image = get_the_post_thumbnail_url($object->ID);
+						if ($image) {
+							$object->post_image = $image;
+						}
+						$wrap->unwrap_object[] = $object;
+					}
 				}
 				$gift->payloads = get_field('payload', $gift->ID);
 				$result['gifts'][] = $gift;
