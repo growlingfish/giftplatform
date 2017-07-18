c0e819ad224319e2bc2d8919ac91e4468386764c
Ben Bedwell <github@growlingfish.com>
2017-07-18 16:31:33 +0100

    API

--- giftplatform.php
+++ giftplatform.php
@@ -292,6 +292,11 @@ function get_gifts ($request) {
 					$wrap->unwrap_personal = get_field('field_594d2552e8835', $wrap->ID);
 					//$wrap->unwrap_object = get_field('object', $wrap->ID);
 					$wrap->unwrap_object = get_field('field_595b4a2bc9c1c', $wrap->ID);
+					if (count($wrap->unwrap_object) > 0) {
+						$wrap->unwrap_object = $wrap->unwrap_object[0];
+					} else {
+						unset($wrap->unwrap_object);
+					}
 					if ($wrap->unwrap_object) {
 						$wrap->unwrap_object->post_image = get_the_post_thumbnail_url($wrap->unwrap_object->ID, 'large');
 						$wrap->unwrap_object->post_content = wpautop($wrap->unwrap_object->post_content);
