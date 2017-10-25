12cf1351e814d1cf91802090bc0436761765da18
Ben Bedwell <github@growlingfish.com>
2017-10-25 16:24:57 +0100

    API

--- giftplatform.php
+++ giftplatform.php
@@ -3,7 +3,7 @@
  * Plugin Name:       GIFT platform plugin
  * Plugin URI:        https://github.com/growlingfish/giftplatform
  * Description:       WordPress admin and server for GIFT project digital gifting platform
- * Version:           0.1.0.0
+ * Version:           0.1.0.1
  * Author:            Ben Bedwell
  * License:           GNU General Public License v3
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
@@ -19,13 +19,12 @@ function giftplatform_activate () {
 	flush_rewrite_rules();
 }
 
-require_once('cred.php');
-
 /*
 *	Place in Wrap custom post type
 */
 
 function giftplatform_enqueue_admin ($hook) {
+	include('cred.php');
     wp_enqueue_script( 'google_maps', 'https://maps.googleapis.com/maps/api/js?key='.GOOGLEMAPSAPI.'&libraries=drawing', array( 'jquery' ) );
 }
 add_action( 'admin_enqueue_scripts', 'giftplatform_enqueue_admin' );
@@ -252,6 +251,10 @@ function gift_v2_register_api_hooks () {
 		'methods'  => 'GET',
 		'callback' => 'get_locations'
 	) );
+	register_rest_route( $namespace.'/v'.$version, '/data/', array(
+		'methods'  => 'GET',
+		'callback' => 'get_data'
+	) );
 	register_rest_route( $namespace.'/v'.$version, '/responses/(?P<id>.+)/', array(
 		'methods'  => 'GET',
 		'callback' => 'get_responses',
@@ -448,6 +451,69 @@ function get_contacts ($request) {
 		'contacts' => $users,
 		'success' => true
 	);
+
+	$response = new WP_REST_Response( $result );
+	$response->set_status( 200 );
+	$response->header( 'Access-Control-Allow-Origin', '*' );
+	
+	return $response;
+}
+
+function get_data ($request) {
+	$query = array(
+		'numberposts'   => -1,
+		'post_type'     => 'gift',
+		'post_status'   => array('publish')
+	);
+	$all_gifts = get_posts( $query );
+	foreach ($all_gifts as $gift) {
+		$hasObject = false;
+		$recipients = get_field( 'field_58e4f6e88f3d7', $gift->ID );
+		foreach ($recipients as $recipient) {
+			$gift->recipient = $recipient;
+			//$gift->wraps = get_field('wrap', $gift->ID);
+			$gift->wraps = get_field('field_58e4f5da816ac', $gift->ID);
+			foreach ($gift->wraps as &$wrap) {
+				//$wrap->unwrap_object = get_field('object', $wrap->ID);
+				$wrap->unwrap_object = get_field('field_595b4a2bc9c1c', $wrap->ID);
+				if (is_array($wrap->unwrap_object) && count($wrap->unwrap_object) > 0) {
+					$wrap->unwrap_object = $wrap->unwrap_object[0];
+				} else if (is_a($wrap->unwrap_object, 'WP_Post')) {
+						
+				} else {
+					unset($wrap->unwrap_object);
+				}  
+				if ($wrap->unwrap_object) {
+					$hasObject = true;
+					$wrap->unwrap_object->post_image = get_the_post_thumbnail_url($wrap->unwrap_object->ID, 'large');
+					$wrap->unwrap_object->post_content = wpautop($wrap->unwrap_object->post_content);
+				}
+			}
+
+			if ($hasObject) {
+				//$gift->payloads = get_field('payload', $gift->ID);
+				$gift->payloads = get_field('field_58e4f689655ef', $gift->ID);
+				foreach ($gift->payloads as &$payload) {
+					$payload->post_content = wpautop($payload->post_content);
+				}
+				//$gift->giftcards = get_field('gift_card', $gift->ID);
+				$gift->giftcards = get_field('field_5964a5787eb68', $gift->ID);
+				foreach ($gift->giftcards as &$giftcard) {
+					$giftcard->post_content = wpautop($giftcard->post_content);
+				}
+				$gift->status = array(
+					//'received' => get_field('received', $gift->ID),
+					'received' => get_field('field_595e186f21668', $gift->ID),
+					//'unwrapped' => get_field('unwrapped', $gift->ID),
+					'unwrapped' => get_field('field_595e0593bd980', $gift->ID),
+					//'responded' => get_field('responded', $gift->ID)
+					'responded' => get_field('field_595e05c8bd981', $gift->ID)
+				);
+				$result['gifts'][] = $gift;
+			}
+			break;
+		}
+	}
 
 	$response = new WP_REST_Response( $result );
 	$response->set_status( 200 );
