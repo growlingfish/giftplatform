56e56106bc9c1ff2f5e94ad079f9ea518c964dc3
Ben Bedwell <github@growlingfish.com>
2018-03-20 20:46:17 +0000

    API

--- giftplatform.php
+++ giftplatform.php
@@ -3,7 +3,7 @@
  * Plugin Name:       GIFT platform plugin
  * Plugin URI:        https://github.com/growlingfish/giftplatform
  * Description:       WordPress admin and server for GIFT project digital gifting platform
- * Version:           0.1.1.5
+ * Version:           0.2.0.0
  * Author:            Ben Bedwell
  * License:           GNU General Public License v3
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
